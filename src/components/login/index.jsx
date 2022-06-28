63506306f264b4551d3c5bc5be82d3111114936c
lauralagares <lauralagares1992@example.com>
2022-06-28 12:15:52 +0200

    fix redirect uri

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -5,7 +5,7 @@ import { BsSpotify } from 'react-icons/bs';
 
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
 
-const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=http://localhost:3000/authentication&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
 
 function Login() {
 
